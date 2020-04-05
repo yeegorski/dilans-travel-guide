84fdcd4db3465f1ae4dfff9bc980f57142a9c12a
Yegor Ganin <63185173+yeegorski@users.noreply.github.com>
2020-04-05 12:00:08 +0200

    Update and rename data_sort_groups.sh to data_sort.sh

--- data_sort.sh
+++ data_sort.sh
@@ -1,10 +1,16 @@
+#!/usr/bin/env bash
+
+#Downloading the dataset to my data server
+
+wget 0.0.0.0/testtask/dilans_data.csv > /home/dataguy/dilan/dilans_data.csv
+
 # Extracting data from dilans_data.csv into the groups
 
 # new readers
 grep 'Reddit\|SEO\|AdWords' /home/dataguy/dilan/dilans_data.csv > /home/dataguy/dilan/new_readers.csv
 
 #returning readers
-grep 'read' /home/dataguy/dilan/dilans_data.csv | grep -v 'Reddit' | grep -v 'SEO' | grep -v 'AdWords' > /home/dataguy/dilan/returning_readers.csv
+grep 'read' /home/dataguy/dilan/dilans_data.csv | grep -v 'Reddit\|SEO\|AdWords' > /home/dataguy/dilan/returning_readers.csv
 
 #subscribers
 grep 'subscribe' /home/dataguy/dilan/dilans_data.csv > /home/dataguy/dilan/subscribers.csv
