6a4af3c1b1487e0d2e2b89fc53915655dbbb6f48
Yegor Ganin <63185173+yeegorski@users.noreply.github.com>
2020-04-05 12:02:06 +0200

    Data download and sorting

--- data_sort.sh
+++ data_sort.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 #Downloading the dataset to my data server
-
 wget 0.0.0.0/testtask/dilans_data.csv > /home/dataguy/dilan/dilans_data.csv
 
 # Extracting data from dilans_data.csv into the groups
